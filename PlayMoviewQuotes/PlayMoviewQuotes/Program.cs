f99dbe05c764297c294e14e44dd452363247baef
Zane <zdegner@gmail.com>
2015-05-27 09:37:59 -0500

    Add text result to sound loop

--- PlayMoviewQuotes/PlayMoviewQuotes/Program.cs
+++ PlayMoviewQuotes/PlayMoviewQuotes/Program.cs
@@ -15,27 +15,33 @@ namespace PlayMovieQuotes
             Random rnd = new Random();
             string entry;
             int num;
+            string text;
             do
             {
                 num = rnd.Next(0, 3);
                 if (num == 0)
                 {
                     result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/holy_sh.wav";
+                    text = "Holy Shit!";
                 }
                 else if (num == 1)
                 {
                     result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/bitchin.wav";
+                    text = "Bitchin...";
                 }
                 else if (num == 2)
                 {
                     result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/good2.wav";
+                    text = "Damn. I'm looking GOOD!";
                 }
                 else
                 {
                     result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/damn.wav";
+                    text = "Damn....";
                 }
                 SoundPlayer sound = new SoundPlayer(result);
                 sound.Play();
+                Console.WriteLine(text);
                 Console.WriteLine("Quit? (N)");
                 entry = Console.ReadLine().ToUpper();
                 Console.Clear();
