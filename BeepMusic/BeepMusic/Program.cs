38e78418a5d87e4427951c9e578d60edd90ce4a4
Zane <zdegner@gmail.com>
2015-05-27 09:23:49 -0500

    Initial Commit

---
+++ BeepMusic/BeepMusic/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Player
+{
+    class BeepMusic
+    {
+
+        static void Main(string[] args)
+        {
+            Song song = new Song();
+            Score score = new Score();
+            string note;
+            string duration;
+            bool start = true;
+            string prompt;
+            Console.WriteLine("Enter Tempo in BPM");
+            score.BPM = Convert.ToInt32(Console.ReadLine());
+
+            while (start)
+            {
+                Console.WriteLine("Enter Note (A-G)");
+                note = Console.ReadLine().ToUpper();
+                Console.WriteLine("Enter note size (SIXTEENTH - WHOLE)");
+                duration = Console.ReadLine().ToUpper();
+                score.AddNote(note, duration);
+                Console.WriteLine("Would you like to add another note? (y / n)");
+                prompt = Console.ReadLine().ToLower();
+                if (prompt == "n" || prompt == "")
+                {
+                    start = false;
+                }
+            }
+
+            song.Play(score);
+        }
+    }
+    class Song
+    {
+        public void Play(Score tune)
+        {
+            foreach (Score.Note n in tune.notes)
+            {
+                Console.Beep((int)n.NoteTone, n.NoteDuration);
+            }
+        }
+        public void Play()
+        {
+            Console.Beep();
+        }
+    }
+    class Score
+    {
+        public int BPM;
+        public List<Note> notes = new List<Note>();
+
+        public void AddNote(string frequency, string time)
+        {
+            Tone tone = (Tone)Enum.Parse(typeof(Tone), frequency);
+            int duration = GetDuration(time);
+            notes.Add(new Note(tone, duration));
+        }
+
+        public enum Tone
+        {
+            REST = 0,
+            GbelowC = 196,
+            A = 220,
+            Asharp = 233,
+            B = 247,
+            C = 262,
+            Csharp = 277,
+            D = 294,
+            Dsharp = 311,
+            E = 330,
+            F = 349,
+            Fsharp = 370,
+            G = 392,
+            Gsharp = 415,
+        }
+        private int GetDuration (string time)
+        {
+            int qt = 60000 / BPM;
+            switch (time)
+            {
+                case "WHOLE":
+                    return (int)qt * 4;
+                case "HALF":
+                    return (int)qt*2;
+                case "QUARTER":
+                    return (int)qt;
+                case "EIGHTH":
+                    return (int)qt / 2;
+                case "SIXTEENTH":
+                    return (int)qt / 4;
+                default:
+                    return 1000;
+            }
+        }
+        public struct Note
+        {
+            Tone toneVal;
+            int durVal;
+            public Note(Tone frequency, int time)
+            {
+                toneVal = frequency;
+                durVal = time;
+            }
+            public Tone NoteTone { get { return toneVal; } }
+            public int NoteDuration { get { return durVal; } }
+    }
+}
+}
---
+++ PlayMoviewQuotes/PlayMoviewQuotes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayMovieQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            Random rnd = new Random();
+            string entry;
+            int num;
+            do
+            {
+                num = rnd.Next(0, 3);
+                if (num == 0)
+                {
+                    result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/holy_sh.wav";
+                }
+                else if (num == 1)
+                {
+                    result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/bitchin.wav";
+                }
+                else if (num == 2)
+                {
+                    result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/good2.wav";
+                }
+                else
+                {
+                    result = "http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/damn.wav";
+                }
+                SoundPlayer sound = new SoundPlayer(result);
+                sound.Play();
+                Console.WriteLine("Quit? (N)");
+                entry = Console.ReadLine().ToUpper();
+                Console.Clear();
+            } while (entry != "N");
+            SoundPlayer end = new SoundPlayer("http://www.wavsource.com/snds_2015-05-24_3842437052126496/video_games/duke/terminated.wav");
+            end.Play();
+            Console.WriteLine("TERMINATED!");
+            Console.ReadKey();
+
+        }
+    }
+}
