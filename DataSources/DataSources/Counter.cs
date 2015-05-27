10bdacf42bdd1e5059a76bc40374aaa621d0871a
Zane <zdegner@gmail.com>
2015-05-27 17:11:38 -0500

    Working on counter, going home for the day

---
+++ DataSources/DataSources/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSources
+{
+    class Counter
+    {
+        
+        public static void CountWords(string entry)
+        {
+            int count = 0;
+            string[] spaced = entry.Split(null);
+            foreach (string s in spaced)
+            {
+                Console.WriteLine(s);
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ DataSources/DataSources/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataSources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string sample = "The quick brown fox jumped over the lazy dog!";
+            string filetext;
+            using (StreamReader text = new StreamReader("C:/Projects/file.txt"))
+            {
+                filetext = text.ReadToEnd();
+                //Console.WriteLine(filetext);
+                Console.WriteLine("End of line");
+            }
+            Counter.CountWords(filetext);
+            Console.ReadKey();
+
+        }
+    }
+}
