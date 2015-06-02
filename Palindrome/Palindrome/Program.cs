0f43b69d2d6c2fe0720b45c6a34c436410df3ba9
Kalfira <zdegner@gmail.com>
2015-06-01 21:10:19 -0500

    Palindrome upload

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entry to check if it's a palindrome:");
+            string entry = Console.ReadLine();
+            var reverse = entry.Reverse();
+
+            if (Check(entry, reverse.ToArray()))
+            {
+                Console.WriteLine("Yes! {0} is a palindrome!", entry);
+            }
+            else
+            {
+                Console.WriteLine("Sorry... {0} is not a palindrome", entry);
+            }
+            Console.ReadKey();
+        }
+
+        static bool Check(string thing, char[] reverse)
+        {
+            bool val = false;
+            int i = 0;
+            Console.WriteLine("Starting");
+            foreach (char letter in thing)
+            {
+                if (letter == reverse[i])
+                {
+                    val = true;
+                }
+                else
+                {
+                    return false;
+                }
+                i++;
+            }
+            return val;
+        }
+    }
+}
