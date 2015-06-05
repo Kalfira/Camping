100a11f77cb53bf2b647ff73f18ac145b99716c2
Kalfira <zdegner@gmail.com>
2015-06-04 22:33:45 -0500

    EoD

---
+++ TotallyLegitLinqPractice/TotallyLegitLinqPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace TotallyLegitLinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] collection =
+            {
+                "Monkey Gundam", "Uh this thing...", "The fantastic Mr. Fox McCloud",
+                "The stuff of my nightmares!"
+            };
+
+            var Query =
+
+                from thing in collection
+                where thing.Length > 5
+                select thing;
+
+            foreach (var col in Query)
+            {
+                Console.WriteLine(col);
+            }
+            Console.Read();
+        }
+    }
+}
