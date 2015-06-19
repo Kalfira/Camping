67b59e6a9c76537b60cd8f4c694ca02d561b475f
Zane <zdegner@gmail.com>
2015-06-19 18:01:36 -0500

    Going home

---
+++ IntroAPI/IntroAPI/Controllers/API/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IntroAPI.Models;
+
+namespace IntroAPI.Controllers.API
+{
+    public class ProductsController : ApiController
+    {
+        private static ConcurrentStack<Product> _products; 
+        // GET: api/Products
+        static ProductsController()
+        {
+            _products = new ConcurrentStack<Product>(new List<Product>
+            {
+                new Product {Name = "Milk", Price = 2.33m},
+                new Product{Name="Cheese", Price = 55.33m},
+                new Product{Name = "Tesla", Price = 8989.33m}
+            });
+        }
+
+        // GET: api/Products/5
+        public IEnumerable<Product> Get()
+        {
+            return _products;
+        }
+
+        // POST: api/Products
+        public HttpResponseMessage Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _products.Push(product);
+                return Request.CreateResponse(HttpStatusCode.Created, product);
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, this.ModelState);
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IntroAPI/IntroAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntroAPI.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "You must enter a product name!")]
+        public string Name { get; set; }
+
+       // [Range(0, 10000, ErrorMessage = "Invalid product price!")]
+        [Required(ErrorMessage = "You must enter a product price!")]
+        public decimal Price { get; set; }
+    }
+}
